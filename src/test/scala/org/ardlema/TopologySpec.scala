c9c1284b0fcb7d97482f65227d6cea1f6185ceea
Alberto Rodríguez <alberto.rodriguez@ust-global.com>
2018-10-01 09:34:11 +0200

    First test skeleton

---
+++ src/test/scala/org/ardlema/TopologySpec.scala
@@ -0,0 +1,19 @@
+package org.ardlema
+
+import org.scalatest.{FunSpec, Matchers}
+
+class TopologySpec extends FunSpec with Matchers {
+
+  describe("The topology") {
+
+    it("should filter the elements") {
+
+      true should be(true)
+    }
+
+    it("should not filter the elements") {
+
+      true should be(true)
+    }
+  }
+}
