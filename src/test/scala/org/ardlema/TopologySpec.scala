b5d6a4a0f5306504838489709ea0bddcc216b3f9
Alberto Rodríguez <alberto.rodriguez@ust-global.com>
2018-10-26 11:10:16 +0200

    Simple input-output redirection test working fine!

--- src/test/scala/org/ardlema/TopologySpec.scala
+++ src/test/scala/org/ardlema/TopologySpec.scala
@@ -1,11 +1,13 @@
 package org.ardlema
 
+import java.lang
 import java.util.Properties
 
 import kafka.server.KafkaConfig
-import org.apache.kafka.common.serialization.{LongDeserializer, StringDeserializer}
-import org.apache.kafka.streams.scala.StreamsBuilder
-import org.apache.kafka.streams.{Topology, TopologyTestDriver}
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams.test.{ConsumerRecordFactory, OutputVerifier}
+import org.apache.kafka.streams.{StreamsBuilder, Topology, TopologyTestDriver}
 import org.ardlema.infra.KafkaInfra
 import org.scalatest.{FunSpec, Matchers}
 
@@ -16,7 +18,7 @@ class TopologySpec extends FunSpec with Matchers with KafkaInfra {
 
   describe("The topology") {
 
-    it("should filter the elements") {
+    it("should send the elements to the output topic") {
       val kafkaConfig = new Properties()
       //TODO: Extract these properties to a dev environment config file!!
       //for now we ips need to be get from containers with inspect
@@ -41,9 +43,15 @@ class TopologySpec extends FunSpec with Matchers with KafkaInfra {
 
 
       withKafkaServerAndSchemaRegistry(Option(kafkaConfig), true) { kafkaServer =>
-        val testDriver = new TopologyTestDriver(TopologyBuilder.createTopology(), kafkaConfig)
 
-        true should be(true)
+        val testDriver = new TopologyTestDriver(TopologyBuilder.createTopology(), kafkaConfig)
+        //val consumerRecord = factory.create("key", new java.lang.Integer(42))
+        val stringDeserializer = new StringDeserializer()
+        val longDeserializer = new LongDeserializer()
+        val recordFactory = new ConsumerRecordFactory(new StringSerializer(), new LongSerializer())
+        testDriver.pipeInput(recordFactory.create("input-topic", "a", 1L, 9999L))
+        val outputRecord: ProducerRecord[String, lang.Long] = testDriver.readOutput("output-topic", new StringDeserializer(), new LongDeserializer())
+        OutputVerifier.compareKeyValue[String, lang.Long](outputRecord, "a", 1L);
       }
     }
   }
@@ -53,6 +61,7 @@ object TopologyBuilder {
 
   def createTopology(): Topology = {
     val builder = new StreamsBuilder()
+    builder.stream("input-topic").to("output-topic")
     builder.build()
   }
 }
